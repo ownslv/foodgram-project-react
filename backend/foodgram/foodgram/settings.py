2ec09ef2c2d174177720653be739643ec13e6cbf
root <root@epdqhrq6kebdd57b1lr5.auto.internal>
2023-08-12 10:29:47 +0000

    fix

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -104,8 +104,8 @@ DATABASES = {
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT')
+        'HOST': env('DB_HOST', default='db'),
+        'PORT': env('DB_PORT', default='5432')
     }
 }
 
